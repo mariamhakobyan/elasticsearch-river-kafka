bb0c44bcaa8af9f24c4950ad2c9794472a28c7ab
Michael Schonfeld <michael@schonfeld.org>
2015-01-20 12:51:39 -0500

    adapt statsagent to new atomic types

--- src/main/java/org/elasticsearch/river/kafka/StatsAgent.java
+++ src/main/java/org/elasticsearch/river/kafka/StatsAgent.java
@@ -13,13 +13,19 @@ public class StatsAgent {
                 riverConfig.getStatsdHost(),
                 riverConfig.getStatsdPort());
         
-        this.keyPrefix = riverConfig.getTopic() + ".";
+        this.keyPrefix = riverConfig.getTopic();
     }
     
     public void logStats(Stats stats) {
-        client.count(keyPrefix + ".failed", stats.failed);
-        client.count(keyPrefix + ".succeeded", stats.succeeded);
-        client.count(keyPrefix + ".flushCount", stats.flushCount);
-        client.count(keyPrefix + ".messagesReceived", stats.messagesReceived);
+        client.count(keyPrefix + ".failed", stats.failed.get());
+        client.count(keyPrefix + ".succeeded", stats.succeeded.get());
+        client.count(keyPrefix + ".flushCount", stats.flushCount.get());
+        client.count(keyPrefix + ".messagesReceived", stats.messagesReceived.get());
+
+        for(Integer partitionId : stats.lastCommitOffsetByPartitionId.keySet()) {
+            client.gauge(
+                    String.format("%s.partition_%s.offset", keyPrefix, partitionId),
+                    stats.lastCommitOffsetByPartitionId.get(partitionId));
+        }
     }
 }
